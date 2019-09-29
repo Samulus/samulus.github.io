b1c4d9314bd74416f38cf0f65525da80210352ba
Samuel Vargas <savarga1@asu.edu>
2019-09-29 00:41:34 -0700

    🎓 Graduated from ASU!

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: [/.css$/],
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: [/.js$/],
+                exclude:
+                    /(node_modules)/,
+                use:
+                    {
+                        loader: 'babel-loader',
+                        options:
+                            {
+                                presets: [
+                                    '@babel/preset-env'
+                                ]
+                            }
+                    }
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]',
+                    outputPath: 'img',
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html",
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+
+        new CopyPlugin([
+            { from: 'src/img/', to: 'img', force: true},
+            { from: 'src/resume/', to: 'resume', force: true}
+        ]),
+    ]
+}
+;
